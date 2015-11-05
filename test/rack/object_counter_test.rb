4a2f711519ffa98b46ca957769cc257fb4af9d5e
yalab <rudeboyjet@gmail.com>
2015-11-05 15:20:35 +0900

    Prepare to test.

--- test/rack/object_counter_test.rb
+++ test/rack/object_counter_test.rb
@@ -1,11 +1,17 @@
 require 'test_helper'
 
 class Rack::ObjectCounterTest < Minitest::Test
+  include Rack::Test::Methods
   def test_that_it_has_a_version_number
     refute_nil ::Rack::ObjectCounter::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def app
+    TestApp.new
+  end
+
+  def test_redirect_logged_in_users_to_dashboard
+    get "/"
+    assert last_response.ok?
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,16 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rack/object_counter'
 
 require 'minitest/autorun'
+require 'rack/test'
+
+
+class TestApp
+  def call(env)
+    greeting = "hello"
+    code   = 200
+    body   = [ greeting ]
+    header = { "Content-Type"           => "text/html;charset=utf-8",
+               "Content-Length"         => greeting.length}
+      [ code, header, body ]
+  end
+end
